98b69311c07787bb86c808fe26397764eebd430a
Devancy <devancy@outlook.com.vn>
2019-03-23 09:17:19 +0700

    update to v1.2.0.0

--- TraCuuThongBaoPhatHanh/Properties/AssemblyInfo.cs
+++ TraCuuThongBaoPhatHanh/Properties/AssemblyInfo.cs
@@ -1,12 +1,13 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TraCuuThongBaoPhatHanh")]
-[assembly: AssemblyDescription("Công cụ tra cứu thông báo phát hành hoá đơn bằng mã số thuế")]
+[assembly: AssemblyTitle("Tra cứu")]
+[assembly: AssemblyDescription("Công cụ tra cứu thông in từ mã số thuế")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Softdreams JSC")]
 [assembly: AssemblyProduct("TraCuuThongBaoPhatHanh")]
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.3")]
-[assembly: AssemblyFileVersion("1.1.0.3")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+
