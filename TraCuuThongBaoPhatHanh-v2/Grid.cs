e9b6a22e90e51c80a5eb8d1dc639833c8fc37d35
Devancy <devancy@outlook.com.vn>
2019-10-26 08:46:50 +0700

    highlight sds

--- TraCuuThongBaoPhatHanh-v2/Grid.cs
+++ TraCuuThongBaoPhatHanh-v2/Grid.cs
@@ -13,7 +13,7 @@ namespace TraCuuThongBaoPhatHanh_v2
 
         public Grid(DataTable dataSource, string elapsed) : this()
         {
-            this.Text = $"Chi tiết thông báo phát hành - Highlight Softdreams [{elapsed}]";
+            this.Text = $"Chi tiết thông báo phát hành - [{elapsed}]";
             this.dataGridView.DataSource = dataSource;
             dataGridView.ColumnHeadersDefaultCellStyle.Font = new Font(DataGridView.DefaultFont, FontStyle.Bold);
             
@@ -24,12 +24,20 @@ namespace TraCuuThongBaoPhatHanh_v2
 
         private void DataGridView_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
         {
-            var tax = dataGridView.Rows[e.RowIndex].Cells["Mã số thuế"]?.Value as string;
-            if(tax != null && tax.Trim() == "0105987432")
-                dataGridView.Rows[e.RowIndex].DefaultCellStyle.ForeColor = Color.OrangeRed;
+            if (dataGridView.Rows[e.RowIndex].Cells["Mã số thuế"]?.Value is string tax && tax.Trim() == "0105987432")
+            {
+                var centerFormat = new StringFormat
+                {
+                    Alignment = StringAlignment.Center,
+                    LineAlignment = StringAlignment.Center
+                };
+                var headerBounds = new Rectangle(e.RowBounds.Left, e.RowBounds.Top, dataGridView.RowHeadersWidth, e.RowBounds.Height);
+                e.Graphics.DrawString("SDS", this.Font, SystemBrushes.ControlText, headerBounds, centerFormat);
 
-            var serial = dataGridView.Rows[e.RowIndex].Cells["Ký hiệu"]?.Value as string;
-            if (serial != null && serial.Trim().EndsWith("E", System.StringComparison.InvariantCultureIgnoreCase))
+                dataGridView.Rows[e.RowIndex].DefaultCellStyle.ForeColor = Color.OrangeRed;
+            }
+
+            if (dataGridView.Rows[e.RowIndex].Cells["Ký hiệu"]?.Value is string serial && serial.Trim().EndsWith("E", System.StringComparison.InvariantCultureIgnoreCase))
             {
                 dataGridView.Rows[e.RowIndex].Cells["Ký hiệu"].Style.ForeColor = dataGridView.DefaultCellStyle.SelectionForeColor;
                 dataGridView.Rows[e.RowIndex].Cells["Ký hiệu"].Style.BackColor = dataGridView.DefaultCellStyle.SelectionBackColor;
